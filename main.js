594213cc1f07a493674fdb280d5caf837afee826
RobertDafny <151602435+RobertDafny@users.noreply.github.com>
2023-11-28 22:00:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Main script
-!window.jQuery	
-    ? alert('Помилка!\nДля правильної роботи програми необхідно підключити бібліотеку: jQuery') 
+!window.jQuery
+    ? alert('Помилка!\nДля правильної роботи програми необхідно в налаштуваннях плагіна увімкнути елементи:\n1. Активно\n2. JavaScript\n3. jQuery')
     : console.log(`Версія jQuery: ${jQuery.fn.jquery}`);
 controllerObj = {
     registerGrabberJs: async function(){
@@ -9,13 +9,15 @@ controllerObj = {
             let script = await fetch(fileUrl).then(response => response.text());
             eval(script);
         } catch(e){
-            return Promise.reject(new Error('Помилка при отриманні файлу граббера'));
+            return Promise.reject(new Error(`Помилка при отриманні файлу граббера\n${e.stack}`));
         }
     }
 }
 controllerObj.registerGrabberJs().then(async () => {
     $(document).ready(async function(){
-	await new Promise(resolve => setTimeout(resolve,2000));
+        await new Promise(resolve => setTimeout(resolve,2000));
+        // Set to true if you need to get new courses
+        pageObj.isActiveGrabber = true;
         await pageObj.init().catch(console.log);
         await pageObj.grabberRun().catch(console.log);
         pageObj.registerListeners();
